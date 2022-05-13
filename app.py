d1edebfc5b3bbbb5af1aa2e4908e070b24094489
ayushsawnani <ayushsawnani@gmail.com>
2022-05-13 07:47:09 -0400

    updated % keyword replace

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def getClassesAndGrades(user, pas):
     for i in pas:
         if i == '=':
             pas = pas.replace(i, '#')
+        elif i == "~":
+            pas = pas.replace(i, '%')
     
 
     b = backend.PortalControl(user, pas)
@@ -27,17 +29,18 @@ def getClassesAndGrades(user, pas):
 
     json_string = {}
 
+    json_string["classes"] = []
+
     for i in range (len(classes)-1):
         print(i)
-        json_string[classes[i]] = []
-        json_string[classes[i]].append({"teacher" : teachers[i], "grade" : grades[i], "letter" : letters[i]})
+        json_string["classes"].append({"class" : classes[i], "teacher" : teachers[i], "grade" : grades[i], "letter" : letters[i]})
 
 
     print (json_string)
 
     json_obj = json.loads(json.dumps(json_string))
     
-    return json.dumps(json_obj, indent=4)
+    return json_obj
     
     
 
